3dea16fcfc9fa0dbd8d9af159f8c1628fb7e345b
sburduli <threeunexpectedperson@gmail.com>
2025-03-04 13:54:39 +0400

    Changed vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  assetsInclude: ['**/*.html','/ui/**/*.css'],
+  assetsInclude: ['**/*.html','/ui/**/*.css', '/ui/**/*.html'],
 });
