eafe79d824d0b4b4f579b0327ab8b8aa60fa15e1
Tsotne Darjania <tsotnedarjania1997@gmail.com>
2025-03-12 13:39:23 +0400

    .

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
 import { defineConfig } from "vite";
-
 export default defineConfig({
+  server: { port: 8004 },
   build: {
     rollupOptions: {
       output: {
         manualChunks(id) {
           if (id.includes("node_modules")) {
             return id
-                .toString()
-                .split("node_modules/")[1]
-                .split("/")[0]
-                .toString();
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
           }
         },
       },
